6f1b68758543f697bc92c259e5da74a145a7d9c6
Alaukik Ram <alaukik.ram@ril.com>
2022-02-16 01:09:46 +0530

    added new questions

---
+++ Relevancy/src/main/java/wrapper/linkedList/FindStartOfLoop.java
@@ -0,0 +1,70 @@
+package wrapper.linkedList;
+
+import static wrapper.linkedList.Reversal.printList;
+
+public class FindStartOfLoop {
+    static LinkedList head;
+
+
+    public static void main(String[] args) {
+        head = new LinkedList(1);
+        head.next = new LinkedList(4);
+        head.next.next = new LinkedList(3);
+        head.next.next.next = new LinkedList(2);
+        head.next.next.next.next = new LinkedList(5);
+        head.next.next.next.next.next = new LinkedList(6);
+        head.next.next.next.next.next.next = new LinkedList(7);
+        head.next.next.next.next.next.next.next = new LinkedList(8);
+        head.next.next.next.next.next.next.next.next =  head.next.next.next.next;
+
+        //printList(head);
+        LinkedList node = findStartOfLoop(head);
+        System.out.println("Loop start point "+ node.value);;
+    }
+
+    private static LinkedList findStartOfLoop(LinkedList head) {
+        boolean isLoop = false;
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        LinkedList slow = head, fast= head;
+
+        //finding loop
+        while(fast != null && fast.next != null){
+            if(slow != head && slow == fast ){
+                isLoop = true;
+                break;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println("fast pointer"+ fast.value);
+        System.out.println("slow pointer"+ slow.value);
+
+        //finding length of the loop
+        int length=1;
+        while(fast.next != slow){
+            length++;
+            fast = fast.next;
+        }
+
+        System.out.println("loop leght is "+ length);
+
+        //finding the start of the loop
+         slow =head;
+         fast = head;
+         for(int i=1; i<=length; i++){
+             fast = fast.next;
+         }
+
+         while(slow != fast){
+             slow = slow.next;
+             fast = fast.next;
+         }
+
+       return slow;
+    }
+
+   }
---
+++ Relevancy/src/main/java/wrapper/linkedList/PalindromeList.java
@@ -0,0 +1,71 @@
+package wrapper.linkedList;
+
+public class PalindromeList {
+    static LinkedList head;
+
+
+    public static void main(String[] args) {
+        head = new LinkedList(0);
+        head.next = new LinkedList(1);
+        head.next.next = new LinkedList(2);
+        head.next.next.next = new LinkedList(1);
+        head.next.next.next.next = new LinkedList(3);
+        head.next.next.next.next.next = new LinkedList(1);
+        head.next.next.next.next.next.next = new LinkedList(2);
+        head.next.next.next.next.next.next.next = new LinkedList(1);
+        head.next.next.next.next.next.next.next.next =  new LinkedList(0);
+
+        //printList(head);
+        Boolean node = isPalindrome(head);
+        System.out.println("Palindrome LinkedList "+ node);;
+    }
+
+    private static boolean isPalindrome(LinkedList head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+
+        //find middle of linkedList
+        LinkedList middle = findMiddle(head);
+
+        LinkedList first = head,second =middle.next;
+        middle.next = null;
+        //reverse second list
+        second  = reverseList(second);
+
+        //compare both list
+        while(first != null && second != null){
+            if(first.value != second.value){
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+
+    private static LinkedList reverseList(LinkedList head) {
+        LinkedList prev=null, curr = head, next=null;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    private static LinkedList findMiddle(LinkedList head) {
+        LinkedList slow = head , fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+}
